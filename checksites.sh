8dc889e338e020434622197652c370cc15b9c0c3
Kenneth Finnegan <kennethfinnegan2007@gmail.com>
2018-09-13 22:49:29 -0700

    Added touch to fix missing old files
    It will now indicate that all the networks are new, instead of just
    causing diff to puke

--- checksites.sh
+++ checksites.sh
@@ -2,7 +2,13 @@
 #
 # Kenneth Finnegan, 2018
 
-for FAC_ID in 547 702
+# List the facility ID numbers, space separated, here
+SET_OF_FAC_ID="547 702"
+# These can be found by looking up a facility of interest and grabbing the
+# number after /fac/ in the URL
+
+
+for FAC_ID in $SET_OF_FAC_ID
 do
 	FAC_NAME="`curl -sG https://peeringdb.com/api/fac/$FAC_ID | jq '.data[].name'`"
 
@@ -14,6 +20,7 @@ do
 	jq -c '.data[] | [ .name, .asn]' |
 	sort >facility.$FAC_ID.new
 	
+	touch facility.$FAC_ID.old
 	diff facility.$FAC_ID.old facility.$FAC_ID.new
 	mv facility.$FAC_ID.new facility.$FAC_ID.old
 done
